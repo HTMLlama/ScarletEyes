e42cf550daf553afaefcfb4fbb36bd2f5c173ec1
Andrew Hughes <social@hugheswd.com>
2017-07-28 12:37:41 -0700

    -Changed spelling and grammar errors.

--- app/src/main/java/com/hugheswd/www/redstory/StoryActivity.kt
+++ app/src/main/java/com/hugheswd/www/redstory/StoryActivity.kt
@@ -23,13 +23,13 @@ class StoryActivity : AppCompatActivity() {
 
         val pageZero = "Where am I? How did I get here? I can feel the cold damp earth below me. I can hear the soft rustle of leaves on the trees. But no animals, no insects, no life. Life. Am I alive? Did I die? My mind is devoid of.....everything.\n" +
                 "\"Oh, good. You're awake.\""
-        val pageOne = "I remember. It all came rushing back. The last thing I saw. Those red eyes. The purr of their voice slicing through the silence like the blade of a will sharpened katana through an enemy. I remember the spray of blood. The hot sticky wetness across my face.\n" +
+        val pageOne = "I remember. It all came rushing back. The last thing I saw. Those scarlet eyes. The purr of their voice slicing through the silence like the blade of a well sharpened katana through an enemy. I remember the spray of blood. The hot sticky wetness across my face.\n" +
                 "\"Are you just going to lay there all day? Get up, %s.\"".format(nobody)
         val pageTwo = "Get up. Seems simple enough. But my body felt so heavy. And my head started to pound with the effort. When I opened my eyes, the dark forest pitched. I slammed my eyes shut again. Trying in vain to will the world to stop spinning. \n" +
                 "\"Jeez, %s. Get a hold of yourself.\"".format(nobody)
         val pageThree = "It is hard to believe this was the same person as before. They used to look and act so sweet. Then again, they always appeared differently depending on who viewed them."
         val pageFour = "And, for me, they always switched between the two."
-        val pageFive = "\"Welcome to world of darkness. You will get used to the red eyes. The reason you feel dizzy right now is because your equilibrium is still adjusting.\\\" \\n\n" +
+        val pageFive = "\"Welcome to world of darkness. You will get used to the scarlet eyes. The reason you feel dizzy right now is because your equilibrium is still adjusting.\" \n" +
                 "\"What am I?\" My voice felt foreign in my throat. \n" +
                 "\"I'd have thought it would have been obvious by now, %s.\" They purred, \"Why, you're a vampire, of course.\"".format(nobody)
 
